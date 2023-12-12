0ec54faec51ab01f96e5eb8f56c187001c6d89c7
fkalpana <support@erpgulf.com>
2023-12-12 09:11:06 +0000

    Added Item Enquiry Report

---
+++ alhoda/alhoda/report/item_enquiry/item_enquiry.js
@@ -0,0 +1,16 @@
+// Copyright (c) 2023, Erpgulf and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Item Enquiry"] = {
+	"filters": [
+		{
+			"fieldname":"item_code",
+			"label":"Item Code",
+			"fieldtype":"Data",
+			"Default":"AB",
+			"width":"100",
+			"reqd":0,
+		},
+
+	]
+};
